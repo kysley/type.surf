9dcdef5bb442f24703f8643ac1c898031261711d
Evan Kysley <evan@kysley.com>
2021-01-21 01:49:21 -0500

    use /play as the singleplayer practice route

--- src/components/compositions/Play/index.tsx
+++ src/components/compositions/Play/index.tsx
@@ -1,30 +1,77 @@
 import React from 'react';
-import {useNavigate} from 'react-router-dom';
 
-import {useSocketConnection} from '../../../hooks/useSocketHandler';
 import {Box} from '../../Box';
+import TypingAreaComposition from '../TypingArea';
+import {Selection} from '../../ListBox/ListBox';
+import {ChevronsRight} from '@styled-icons/feather';
+import {Stack} from '../../Stack';
+import {useRecoilState, useSetRecoilState} from 'recoil';
+import {ModeState, OrbitState} from '../../../state/state';
 
 export const Play = () => {
-  const {socket} = useSocketConnection();
-  const navigate = useNavigate();
+  const [orbitState, setOrbitState] = useRecoilState(OrbitState);
+  const [modeState, setModeState] = useRecoilState(ModeState);
   return (
     <Box
-      display="grid"
-      gridTemplateColumns="1fr 1fr"
-      gridGap="3em"
-      gridArea="main"
-      onClick={() =>
-        socket.emit('client.queue', 'RACE', (roomId: string) => {
-          navigate(`/p/${roomId}`);
-        })
-      }
+      style={{position: 'relative'}}
+      width="55%"
+      height="auto"
+      justifyContent="center"
+      justifySelf="center"
+      display="flex"
+      flexDirection="column"
     >
-      <Box bg="primary">
-        <h2>Race</h2>
-      </Box>
-      <Box>
-        <h2>Circuit (soon)</h2>
-      </Box>
+      <Stack
+        direction="row"
+        flexDirection="row"
+        justifyContent="center"
+        alignItems="center"
+        pb="2em"
+      >
+        <Selection
+          callback={(v) => setModeState(v)}
+          options={[
+            {label: 'Time', value: 'time'},
+            {label: 'Words', value: 'words'},
+          ]}
+          defaultValue={modeState}
+        />
+        <ChevronsRight height="24px" strokeWidth="2px" />
+        <Selection
+          callback={(v) => setOrbitState(Number(v))}
+          options={[
+            {label: '30', value: '30'},
+            {label: '60', value: '60'},
+          ]}
+          defaultValue={orbitState.toString()}
+        />
+      </Stack>
+      <TypingAreaComposition />
     </Box>
   );
 };
+
+// export const Play = () => {
+//   const {socket} = useSocketConnection();
+//   const navigate = useNavigate();
+//   return (
+//     <Box
+//       display="grid"
+//       gridTemplateColumns="1fr 1fr"
+//       gridGap="3em"
+//       gridArea="main"
+//       onClick={() =>
+//         socket.emit('client.queue', 'RACE', (roomId: string) => {
+//           navigate(`/p/${roomId}`);
+//         })
+//       }
+//     >
+//       <Box bg="primary">
+//         <h2>Race</h2>
+//       </Box>
+//       <Box>
+//         <h2>Circuit (soon)</h2>
+//       </Box>
+//     </Box>
+//   );
+// };
