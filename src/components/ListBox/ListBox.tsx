1f0e80956b50087b7a4a65909602a5856d9050b1
Evan Kysley <evan@kysley.com>
2021-01-21 01:47:44 -0500

    update listbox styling

--- src/components/ListBox/ListBox.tsx
+++ src/components/ListBox/ListBox.tsx
@@ -1,45 +1,88 @@
-import React, {useState, useMemo, useCallback} from 'react';
-import {color, space} from 'styled-system';
+import React, {useState, useCallback} from 'react';
 import {
   ListboxInput,
   ListboxButton,
   ListboxPopover,
   ListboxList,
   ListboxOption,
-  Listbox,
 } from '@reach/listbox';
 import styled from 'styled-components';
+import css from '@styled-system/css';
 
 import '@reach/listbox/styles.css';
+import {CheckCircle} from '@styled-icons/feather';
 
-const StyledListboxInput = styled(ListboxPopover)`
-  ${color};
-  ${space};
-  /* border-radius: 4px; */
-  outline: 0;
-  border: none;
-`;
+const StyledListboxPopover = styled(ListboxPopover)(
+  css({
+    outline: '0 !important',
+    border: 'none',
+    bg: 'background2',
+    mt: '1em',
+    borderRadius: '4px',
+    boxShadow: 'none !important',
+  }),
+);
 
-type Option<T> = {
+const StyledListboxButton = styled(ListboxButton)(
+  css({
+    outline: 'none !important',
+    border: 'none',
+    fontSize: '2rem',
+    padding: '0.5rem 1rem',
+    bg: 'background2',
+    cursor: 'pointer',
+    color: 'text',
+    boxShadow: 'default',
+    borderRadius: '4px',
+    transition: 'all .1s ease-in-out',
+    ':hover': {
+      boxShadow: 'active',
+    },
+    '&[aria-expanded="true"]': {
+      boxShadow: 'active',
+      bg: 'background3',
+    },
+  }),
+);
+
+const StyledListboxList = styled(ListboxList)(
+  css({
+    p: '0.5rem 0.75rem',
+  }),
+);
+
+const StyledListboxOption = styled(ListboxOption)(
+  css({
+    color: 'text',
+    p: '0.5em 2.5em 0.5em 0.5em',
+    cursor: 'pointer',
+    borderRadius: '4px',
+    '&[aria-selected="true"]': {
+      bg: 'background3',
+    },
+  }),
+);
+
+type Option = {
   value: string;
   key?: string;
   label: string;
-  payload?: T;
+  payload?: any;
 };
 
-type SelectionProps<T> = {
-  callback: (value: T | string) => void;
+type SelectionProps = {
+  callback: (value: Option['value']) => void;
   defaultValue?: string;
-  options: Option<T>[];
+  options: Option[];
   passPayload?: boolean;
 };
 
-export function Selection<T>({
+export function Selection({
   callback,
   defaultValue = '',
   options,
   passPayload = false,
-}: SelectionProps<T>) {
+}: SelectionProps) {
   const [value, setValue] = useState(defaultValue);
 
   const handleChange = useCallback(
@@ -56,25 +99,30 @@ export function Selection<T>({
   );
 
   return (
-    <div>
-      <ListboxInput
-        // color="blue"
-        onChange={handleChange}
-        value={value}
-        // bg="yellow"
-        // defaultValue={defaultValue}
-      >
-        <ListboxButton arrow="v" style={{border: 'none'}} />
-        <StyledListboxInput>
-          <ListboxList>
-            {options.map((opt, idx) => (
-              <ListboxOption value={opt.value} key={opt.key || opt.value}>
-                {opt.label || opt.value}
-              </ListboxOption>
-            ))}
-          </ListboxList>
-        </StyledListboxInput>
-      </ListboxInput>
-    </div>
+    <ListboxInput
+      onChange={handleChange}
+      value={value}
+      // defaultValue={defaultValue}
+    >
+      <StyledListboxButton
+      // arrow={<ArrowDown height="30px" strokeWidth={2} />}
+      />
+      <StyledListboxPopover>
+        <StyledListboxList>
+          {options.map((opt, idx) => (
+            <StyledListboxOption value={opt.value} key={opt.key || opt.value}>
+              {opt.label || opt.value}
+              {value === opt.value && (
+                <CheckCircle
+                  height="15px"
+                  strokeWidth="3px"
+                  style={{paddingLeft: '0.5em'}}
+                />
+              )}
+            </StyledListboxOption>
+          ))}
+        </StyledListboxList>
+      </StyledListboxPopover>
+    </ListboxInput>
   );
 }
