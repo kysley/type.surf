73af7a31c44ec9f8554b473f6faeea94b2e370e4
Evan Kysley <evan@kysley.com>
2021-03-29 00:16:08 -0400

    mock guest for ui

--- src/hooks/api/useMe.ts
+++ src/hooks/api/useMe.ts
@@ -2,6 +2,12 @@ import toast from 'react-hot-toast';
 
 import {useMeQuery} from '../../graphql/gen';
 
+const mockUser = {
+  username: 'Guest',
+  discriminator: Math.floor(Math.random() * 9999),
+  id: Math.floor(Math.random() * 9999) + 9999,
+};
+
 export function useMe() {
   const [result, reexec] = useMeQuery();
 
@@ -9,7 +15,7 @@ export function useMe() {
 
   console.log('me');
   return {
-    user: data?.me,
+    user: data?.me || mockUser,
     fetching,
     error,
     reexec,
